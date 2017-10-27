eb31eceb219c2a75c7a26152589ffb7a543f1052
Phone Thant Ko <mr.ptk10598@gmail.com>
2017-10-27 13:56:57 +0800

    Update

--- js/second-round.js
+++
@@ -1,9 +0,0 @@
- var el = document.querySelector("#clipchamp-button");
-  // For more customization options refer to our Documentation.
-  var options = {
-      output: "azure",
-        style: {
-            url:   "https://api.clipchamp.com/static/button/themes/modern-dark.css"
-       }
-  };
-  clipchamp(el, options);
