e2bc2d4da749def8c1b807ee7294983eaa8df162
Deddy T <deddybuster@hotmail.com>
2019-10-03 19:53:26 +0800

    Update apply.js

--- js/apply.js
+++ js/apply.js
@@ -2,6 +2,7 @@ function displayBox(id,box)
 {
     var option = document.getElementById(id);
     var optvalue = option.options[option.selectedIndex].value;
-    if(optvalue == 'yes')
+    if(optvalue == 'yes'){
         document.getElementById(box).style.display = "block";
+    }
 }
