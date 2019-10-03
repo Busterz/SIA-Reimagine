560ad304475cfd0635ac92dbdbd9906302252ebe
Deddy T <deddybuster@hotmail.com>
2019-10-03 19:57:36 +0800

    Update apply.js
    Streamlined if conditional statement

--- js/apply.js
+++ js/apply.js
@@ -2,7 +2,6 @@ function displayBox(id,box)
 {
     var option = document.getElementById(id);
     var optvalue = option.options[option.selectedIndex].value;
-    if(optvalue == 'yes'){
+    if(optvalue == 'yes')
         document.getElementById(box).style.display = "block";
-    }
 }
